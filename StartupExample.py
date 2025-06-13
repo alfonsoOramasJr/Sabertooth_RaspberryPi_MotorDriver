92ab0a093333e119a448c68a9e2a3f604e6c0e60
workordeath <alfonso.oramas@icloud.com>
2025-06-13 16:34:16 -0400

    NEW: Example code for moving the motors for a second before stopping them promptly.

---
+++ StartupExample.py
@@ -0,0 +1,10 @@
+import time
+from MotorDriver import MotorDriver
+
+motor_driver = MotorDriver()
+
+motor_driver.move_motor(1, 5)
+motor_driver.move_motor(2, 5)
+time.sleep(1)
+motor_driver.move_motor(1, 0)
+motor_driver.move_motor(2, 0)
