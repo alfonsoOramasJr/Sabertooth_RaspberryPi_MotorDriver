e2542043053eed317efd58dd3b730bb7aa177921
workordeath <alfonso.oramas@icloud.com>
2025-06-13 16:23:25 -0400

    NEW: Grabs the motor ranges based on the id, simple function we're going to use to send a specific throttle to our two motors.

--- MotorDriver.py
+++ MotorDriver.py
@@ -24,3 +24,10 @@ class MotorDriver:
     def map_value(self, input_value, input_minimum, input_maximum, output_minimum, output_maximum):
         return output_minimum + (input_value - input_minimum) * (output_maximum - output_minimum) / (input_maximum - input_minimum)
     
+    def get_motor_ranges(self, motor_id):
+        try:
+            motor_range_minimum, motor_range_maximum = self.motor_ranges[motor_id]
+            return motor_range_minimum, motor_range_maximum
+        except KeyError:
+            raise ValueError(f"Invalid motor_id: {motor_id}")
+
